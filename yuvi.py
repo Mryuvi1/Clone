9162bf907b4dec7a1f10496c031b5b64d16bd3e0
Mryuvi1 <137717168+Mryuvi1@users.noreply.github.com>
2024-11-22 01:43:56 +0530

    Update yuvi.py

--- yuvi.py
+++ yuvi.py
@@ -1,16 +1,17 @@
-import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/HdppaiQyFtZ1AwiOzc5iEe')
-try:
- import requests
-except:os.system("pip uninstall requests -y;pip install requests")
+import platform
+import OLD_enc
 
-print('[*] Loeading Chacking Update.')
-os.system('git pull --quiet 2>/dev/null')
-bit = platform.architecture()[0]
-if bit == '64bit':
- print('[✓] Found 64 Bit Device')
- import MHB64
-elif bit == '32bit':
- print('\033[1;97m[✓] SORRY BRO YOUR DEVIS IS 32 BIT ')
- print('\033[1;97m[✓] PLEASE USE 4=64 DEVIS ')
- 
+def main():
+    architecture = platform.architecture()[0]
+    if architecture == '32bit':
+        print("Your device is 32-bit.")
+    elif architecture == '64bit':
+        print("Your device is 64-bit.")
+    else:
+        print("Unknown architecture.")
+
+    # Run the OLD_enc module
+    OLD_enc.main()
+
+if __name__ == "__main__":
+    main()
